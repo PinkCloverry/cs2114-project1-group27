e231206e213b6e1c33d5bd7d0d33a982a214773d
Josiah Bernard Samuel <josiahsamuel7284@gmail.com>
2026-09-21 13:16:00 -0400

    Add Card tests

---
+++ CardTest.java
@@ -0,0 +1,34 @@
+public class CardTest {
+
+    public static void main(String[] args) {
+
+        // Test getValue() with a normal card
+        Card ten = new Card(10, false);
+
+        if (ten.getValue() == 10) {
+            System.out.println("PASS: getValue()");
+        } else {
+            System.out.println("FAIL: getValue()");
+        }
+
+        // Test isAce() with an ace
+        Card ace = new Card(11, true);
+
+        if (ace.isAce()) {
+            System.out.println("PASS: isAce() true");
+        } else {
+            System.out.println("FAIL: isAce() true");
+        }
+
+        // Test isAce() with a non-ace
+        Card seven = new Card(7, false);
+
+        if (!seven.isAce()) {
+            System.out.println("PASS: isAce() false");
+        } else {
+            System.out.println("FAIL: isAce() false");
+        }
+
+        System.out.println("Testing complete.");
+    }
+}
